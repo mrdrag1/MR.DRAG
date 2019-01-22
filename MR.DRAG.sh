27b1d2d8667fb4ca1b8c65557392ab33ed8fd9d4
mrdrag1 <46041450+mrdrag1@users.noreply.github.com>
2019-01-22 17:18:30 +0700

    Create MR.DRAG.sh

---
+++ MR.DRAG.sh
@@ -0,0 +1,558 @@
+# ## SCRIPT RUSAK !!! BELUM PERBAIKAN
+
+#! / bin / bash
+
+# versi 1.0
+
+
+pembaruan pkg
+
+peningkatan pkg
+
+pkg instal git
+
+pkg instal php
+
+pkg instal figlet
+
+pkg pasang toilet
+
+pkg instal python2
+
+pkg instal lolcat
+
+permintaan instalasi pip2
+
+pip2 instal termcolor
+
+
+python restart.py
+
+bersih
+
+printf " \ e [101m \ e [1; 77m ::::::::: SELAMAT DATANG ::::::::: \ e [0m \ n "
+
+printf " \ e [101m \ e [1; 77m ;; SEMOGA SELAMAT SAMPAI TUJUAN :: \ e [0m \ n "
+
+
+python2 pwds.py
+
+
+# Variabel
+
+b = ' \ 033 [1 m '
+
+u = ' \ 033 [4m '
+
+bl = ' \ E [30m '
+
+r = ' \ E [31m '
+
+g = ' \ E [32m '
+
+bu = ' \ E [34m '
+
+m = ' \ E [35m '
+
+c = ' \ E [36m '
+
+w = ' \ E [37m '
+
+endc = ' \ E [0m '
+
+enda = ' \ 033 [0m '
+
+blue = ' \ e [1; 34m '
+
+cyan = ' \ e [1; 36m '
+
+red = ' \ e [1; 31m '
+
+
+figlet MIX-Tools | lolcat
+
+
+tidur 1
+
+
+echo -e $ g " ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ " | lolcat
+
+echo -e $ g " ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ " | lolcat
+
+echo -e $ g " ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ " | lolcat
+
+echo -e $ g " ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ " | lolcat
+
+echo -e $ g " ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ " | lolcat
+
+echo -e $ g " ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ ▒▒▒ " | lolcat
+
+echo -e $ bl " . "
+
+echo -e $ bu " ________________________________________ "
+
+echo -e $ bu " Alat: MIX-TOOL $ white " 
+
+echo -e $ bu " Penulis: All Master Termux etc $ white " 
+
+echo -e $ bu " Hubungi: 081235862 *** $ white " 
+
+
+echo -e $ bu " _______________________________________ " 
+
+tidur 1
+
+# ############################################## #
+
+# CTRL + C
+
+# ############################################## #
+
+menjebak ctrl_c INT
+
+ctrl_c () {
+
+bersih
+
+echo -e $ red " ~~> oke, ... "
+
+echo -e $ cyan " .. "
+
+tidur 1
+
+gema " "
+
+echo -e $ r " ... "
+
+tidur 1
+
+}
+
+
+lagi = 1
+
+sementara [ $ lagi -lt 20] ;
+
+melakukan
+
+
+tidur 1
+
+echo -e $ g " ༎༎༎༎༎༎༎༎༎ MENU MR.DRAG ༎༎༎༎༎༎༎༎༎ "
+
+tidur 2
+
+echo -e $ g " ++++++++++++++++++++++++++++++ "
+
+echo -e $ g " [1] PHISING V1 $ {enda} " ;
+
+echo -e $ g " [2] PHISING V2 $ {endc} " ;
+
+echo -e $ g " [3] PHISING V3 $ {endc} " ;
+
+echo -e $ g " [4] PHISING V4 $ {endc} " ;
+
+echo -e $ g " [5] PERMAINAN PHISING $ {endc} " ;
+
+echo -e $ g " [6] Retas target fb $ {endc} " ;
+
+echo -e $ g " [7] Hack fb massal " ;
+
+echo -e $ g " [8] Hack fb Target + Massal " ;
+
+echo -e $ g " [9] Retas FB ans (#root) " ;
+
+echo -e $ g " [10] Retas Instagram (#root) " ;
+
+echo -e $ g " [11] Hack Twitter (#root) " ;
+
+echo -e $ g " [12] Hack Gmail (#root) " ;
+
+echo -e $ g " [13] Info Fb " ;
+
+echo -e $ g " [14] Santet Online " ;
+
+echo -e $ g " [15] Spam IG " ;
+
+echo -e $ g " [16] Spam WA " ;
+
+echo -e $ g " [17] Spam Sms " ;
+
+echo -e $ g " [18] Youtube AutoView (#root) " ;
+
+echo -e $ g " [19] Tembak Kuota XL " ;
+
+echo -e $ g " [20] Tembak Cewe (khusus jones: v) " ;
+
+echo -e $ g " [21] Cara Guna Tool / Tutorial Singkat -_- " ;
+
+echo -e $ g " [0] Modar / Exit " ;
+
+gema " "
+
+echo -e $ bu " Pilih Sesuai Selera Anda :) " 
+
+echo -e $ bu " ketik (TT) untuk cara guna / tutor "
+
+baca -p " ====> " pil ;
+
+figlet MR MBEST | lolcat
+
+# phs shelpis
+
+
+case $ pil masuk
+
+1) pembaruan tepat
+
+apt upgrade
+
+git clone https://github.com/thelinuxchoice/shellphish.git
+
+cd shellphish
+
+bash shellphish.sh
+
+
+;;
+
+
+# phs be
+
+
+2) pembaruan apt
+
+apt upgrade
+
+git clone https://github.com/thelinuxchoice/blackeye.git
+
+cd blackeye
+
+bash blackeye.sh
+
+
+;;
+
+
+# phs socialfis
+
+
+3) pembaruan tepat
+
+apt upgrade
+
+git clone https://github.com/UndeadSec/SocialFish.git
+
+cd SocialFish
+
+chmod + x *
+
+pip2 instal -r requirement.txt
+
+python2 SocialFish.py
+
+
+;;
+
+
+# phs weeman
+
+
+4) git klon 
+
+https://github.com/evait-security/weeman.git
+
+chmod + x *
+
+python2 weeman.py
+
+
+;;
+
+
+# hack ig
+
+
+10) git klon 
+
+https://github.com/thelinuxchoice/instashell.git
+
+cd instashell
+
+bash instashell.sh
+
+tidur 1
+
+echo " root dulu "
+
+
+;;
+
+
+15) pembaruan tepat
+
+apt upgrade
+
+untuk menginstal git
+
+git klon 
+
+https://github.com/thelinuxchoice/instaspam.git
+
+ls
+
+cd instaspam
+
+bash instaspam.sh
+
+
+;;
+
+
+20) gema " lu jones? Santai bro .. "
+
+echo " sabun masih banyak; v "
+
+
+;;
+
+
+11) git klon https://github.com/thelinuxchoice/tweetshell
+
+cd tweetshell
+
+chmod + x tweetshell.sh
+
+bash tweetshell.sh
+
+tidur 2
+
+echo -e $ g " root dulu mbod "
+
+
+;;
+
+
+12) pembaruan && apt yang tepat
+
+git clone https://github.com/thelinuxchoice/gmailshell.git
+
+cd gmailshell
+
+bash gmailshell.sh
+
+tidur 1
+
+echo -e $ g " root dulu, baru bisa jalan tod "
+
+
+;;
+
+
+9) pembaruan && apt yang tepat
+
+git clone https://github.com/thelinuxchoice/facebash.git
+
+cd facebash
+
+bash facebash.sh
+
+tidur 2
+
+echo -e $ g " root dulu kalo mau akses tool ini mbod "
+
+
+;;
+
+
+7) pip2 instal mekanisasi
+
+permintaan instalasi pip2
+
+pip2 install --upgrade pip
+
+git clone https://github.com/alpian9890/bruteforce-hack-fb.git
+
+cd bruteforce-hack-fb.git
+
+python2 MBF.py
+
+
+;;
+
+
+6) pip2 instal --upgrade pip
+
+git clone https://github.com/Senitopeng/fbbrute.git
+
+cd fbbrute
+
+python2 jomblo.py
+
+
+;;
+
+
+8) pip2 instal --upgrade pip
+
+Instal pip2 mekanis
+
+permintaan instalasi pip2
+
+git clone https://github.com/Senitopeng/KumpulanFbbrute.git
+
+cd KumpulanFbbrute
+
+python2 jomblo.py
+
+
+;;
+
+
+19) permintaan instalasi pip2
+
+pkg instal python
+
+pkg instal git
+
+permintaan instalasi pip
+
+git clone https://github.com/albertoanggi/xl-py.git
+
+cd xl-py
+
+python app.py
+
+
+;;
+
+
+17) permintaan instalasi pip2
+
+ untuk menginstal nano
+
+ untuk menginstal git
+
+ git clone https://github.com/Senitopeng/SpamSms.git
+
+ cd SpamSms
+
+ chmod + x mantan.py 
+
+ python2 mantan.py
+
+
+;;
+
+
+16) apt pembaruan -y
+
+apt -y upgrade
+
+untuk menginstal git
+
+untuk memasang php
+
+git clone https://github.com/siputra12/prank
+
+prank cd
+
+ls
+
+php wa.php
+
+
+;;
+
+
+13) git klon https://github.com/CiKu370/OSIF.git
+
+cd OSIF
+
+python2 osif.py
+
+
+;;
+
+
+14) pkg instal python
+
+git klon 
+
+https://github.com/Gameye98/santet-online.git
+
+cd santet-online
+
+python santet.py
+
+
+;;
+
+
+18) pembaruan tepat
+
+apt upgrade
+
+untuk menginstal python
+
+untuk menginstal git
+
+git clone https://github.com/thelinuxchoice/youbot.git
+
+cd youbot
+
+layanan untuk memulai
+
+sudo ./youbot.sh URL
+
+
+;;
+
+
+5) pembaruan && apt yang tepat
+
+untuk menginstal python2
+
+untuk memasang apache2
+
+untuk menginstal git php unzip
+
+git clone https://github.com/Senitopeng/PhisingOnline.git
+
+cd PhisingOnline
+
+ls
+
+unzip PhisingOnline.zip
+
+python2 online.py
+
+
+;;
+
+
+21) guru les
+
+;;
+
+0) echo -e $ bu " KHO - " 
+
+echo -e $ bu " TERIMA KASIH "
+
+printf " \ e [103m \ e [1; 77mPenyusun; MR.DRAG \ e [0m \ n "
+
+tidur 2
+
+keluar
+
+;;
+
+
+* ) echo " PILIH YG BENER CUK !! " | lolcat
+
+esac
+
+selesai
+
+selesai
+
